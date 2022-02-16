b8879cc5637066583813696e9cb9027966f16a3d
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-02-16 21:10:47 +0530

    Create not_requested_message.js

---
+++ not_requested_message.js
@@ -0,0 +1,37 @@
+//updateShort#78d4dec1 update:Update date:int = Updates;
+const _0x78d4dec1 = function(ob){
+		document.getElementById('tgresult').appendChild(renderjson(arguments[0],"updateShort"));
+}
+not_requested_msg["0x78d4dec1"]=_0x78d4dec1
+
+//updates#74ae4240 updates:Vector<Update> users:Vector<User> chats:Vector<Chat> date:int seq:int = Updates;
+const _0x74ae4240 = function(ob){
+		document.getElementById('tgresult').appendChild(renderjson(arguments[0],"updates"));
+}
+not_requested_msg["0x74ae4240"]=_0x74ae4240
+
+//updateShortMessage#914fbf11 flags:# out:flags.1?true mentioned:flags.4?true media_unread:flags.5?true silent:flags.13?true id:int user_id:int message:string pts:int pts_count:int date:int fwd_from:flags.2?MessageFwdHeader via_bot_id:flags.11?int reply_to_msg_id:flags.3?int entities:flags.7?Vector<MessageEntity> = Updates;
+const _0x914fbf11 = function(ob){
+		document.getElementById('tgresult').appendChild(renderjson(arguments[0],"updateShortMessage"));
+}
+not_requested_msg["0x914fbf11"]=_0x914fbf11
+
+const _0x347773c5 = function(ob){
+	//document.getElementById('tgresult').appendChild(renderjson(arguments[0],"Pong "));
+}
+not_requested_msg["0x347773c5"]=_0x347773c5
+
+const _0xf35c6d01 = function(ob){
+	//document.getElementById('tgresult').appendChild(renderjson(arguments[0],"RPCResult "));
+}
+not_requested_msg["0xf35c6d01"]=_0xf35c6d01
+
+const _0x62d6b459 = function(ob){
+	//document.getElementById('tgresult').appendChild(renderjson(arguments[0],"MsgAsk "));
+}
+not_requested_msg["0x62d6b459"]=_0x62d6b459
+
+const _0xa7eff811 = function(ob){//bad_msg_notification#a7eff811 bad_msg_id:long bad_msg_seqno:int error_code:int = BadMsgNotification;
+	//document.getElementById('tgresult').appendChild(renderjson(arguments[0],"BadMsgNotification "));
+}
+not_requested_msg["0xa7eff811"]=_0xa7eff811
