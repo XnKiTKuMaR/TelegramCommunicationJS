656928d152f4cbce564a378e4dd81f36fc57239c
⚝αикιт кυмαя⚝ <98248106+xNKIT@users.noreply.github.com>
2022-02-16 21:02:36 +0530

    Create git.php

---
+++ git.php
@@ -0,0 +1,11 @@
+<?php
+// xnkit Base
+// https://github.com/xnkit/TelegramCommunication
+for($i=11;$i<=46;$i++) {
+	exec("find . -type d -name '*_cache' -exec rm -r {} +");
+	exec("unzip -o ".$i.".zip");
+	exec("rm readme");
+	exec("git add .");
+	exec("git commit -m \"Part ".$i."\"");
+	exec("git push");
+}
